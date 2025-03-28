44163d9ea4688c25272e41be03646f22ea9b2e45
Leonardo Souzas <leonardomellolopes@gmail.com>
2025-03-27 21:41:36 -0300

    primeiras alterações feitas no front v1

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -7,11 +7,9 @@ export default function Home() {
     return (
         <Box bg="#FFFfff" h="100vh">
             {/* Cabeçalho */}
-            <Box bg="#F7931E" py={5} px={6} display="flex" alignItems="center" justifyContent="space-between" position="fixed" zIndex="1" width="100%" height="8%" border="none">
-                <Image src={heroImg} alt="Logo" boxSize="90px" />
-                <Button mt={5} colorScheme="teal" size="lg" display="flex" bg="orange.400" color="white" _hover={{ bg: "#6A9C89", transform: "scale(1.1)", boxShadow: "md" }} >
-                    COMO FUNCIONA?
-                </Button>
+            <Box bg="#F7931E" px={6} display="flex" alignItems="center" justifyContent="space-between" position="fixed" zIndex="1" width="100%" height="10%" border="none">
+                <Box height="115%"> <Image src={heroImg} alt="Logo" height="100%" width="100%" justifyContent="center" /> </Box>
+                <Box height="100%"> <Button mt={5} colorScheme="teal" size="2xl" display="flex" bg="orange.400" color="white" _hover={{ bg: "#6A9C89", transform: "scale(1.1)", boxShadow: "md" }} >COMO FUNCIONA?</Button> </Box>
             </Box>
 
             {/* Conteúdo principal */}
@@ -21,13 +19,12 @@ export default function Home() {
                         Seu momento de relaxar <br />  na palma da sua mão!
                     </Heading>
                     <Button mt={5} colorScheme="teal" size="2xl" fontSize="1.5rem" bg="#F7931E" color="white" border="1px, solid" _hover={{ color: "white", bg: "#6A9C89", transform: "scale(1.1)", boxShadow: "md", }} borderRadius="10px">
-                        Agende Seu Horário  
+                        Agende Seu Horário
                     </Button>
                 </Box>
                 <Image
                     src="/src/assets/a.jpg"
                     borderRadius="full"
-
                     boxSize="500px"
                     objectFit="cover"
                     alt="Fisioterapeuta"
